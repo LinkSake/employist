c3c102ba7cc660bc54a8873a9cdb0e9a8c208c0b
Luis Angel Ortega <angel-ortega12@hotmail.com>
2020-10-31 18:46:57 -0700

    ✨ Add an app-name header

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { React, useState } from 'react';
 import NewConrtainer from './new/NewContainer';
-import { Button, Grid } from 'semantic-ui-react';
+import { Button, Header, Icon, Grid } from 'semantic-ui-react';
 import TableContainer from './table/TableContainer';
 
 const App = () => {
@@ -19,23 +19,32 @@ const App = () => {
     <Grid columns='16'>
         <Grid.Column computer='2' tablet='1' mobile='1'/>
         <Grid.Column computer='12' tablet='16' mobile='16'>
-          <Grid.Row>
-            <Grid columns='12'>
-              <Grid.Column computer='8' tablet='8' mobile='16'>
-                <NewConrtainer 
-                modalState={modalState} 
-                employees={employeeList} 
-                setModalState={setModalState}
-                setEmployees={setEmployeeList}/>
-              </Grid.Column>
-              <Grid.Column computer='8' tablet='8' mobile='16'>
-                <Button fluid>Change currency</Button>
-              </Grid.Column>
-            </Grid>
-          </Grid.Row>
-          <Grid.Row>
-            <TableContainer employees={employeeList}/>
-          </Grid.Row>
+            <Grid.Row className='py-5'>
+                <Header textAlign='center' as='h2' icon>
+                    <Icon name='address book' />
+                        Employist
+                    <Header.Subheader>
+                        Manage your employees in a easy way!
+                    </Header.Subheader>
+                </Header>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid columns='12'>
+                    <Grid.Column computer='8' tablet='8' mobile='16'>
+                        <NewConrtainer 
+                        modalState={modalState} 
+                        employees={employeeList} 
+                        setModalState={setModalState}
+                        setEmployees={setEmployeeList}/>
+                    </Grid.Column>
+                    <Grid.Column computer='8' tablet='8' mobile='16'>
+                        <Button fluid>Change currency</Button>
+                    </Grid.Column>
+                </Grid>
+            </Grid.Row>
+            <Grid.Row>
+                <TableContainer employees={employeeList}/>
+            </Grid.Row>
         </Grid.Column>
         <Grid.Column computer='2' tablet='1' mobile='1'/>
     </Grid>
