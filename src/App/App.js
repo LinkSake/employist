5e345e76a976405ff507c8e12db4959f0c9671ee
Luis Angel Ortega <angel-ortega12@hotmail.com>
2020-10-31 19:16:19 -0700

    ✨ Added coin and headers

--- src/App/App.js
+++ src/App/App.js
@@ -12,8 +12,23 @@ const App = () => {
     { id: 2, name: 'Rodrigo', last: 'Ka', company: 'Corp. Co.', img: '', wage: 1998 },
   ]
 
-  const [employeeList, setEmployeeList] = useState(tempEmploy);
+  const [coin, setCoin] = useState('MXN')
   const [modalState, setModalState] = useState(false);
+  const [employeeList, setEmployeeList] = useState(tempEmploy);
+
+  const changeCoin = () => {
+      if (coin === 'MXN') {
+        setCoin('USD')
+        employeeList.forEach(empl => {
+            empl.wage = empl.wage / 21.50;
+        });
+      } else {
+        setCoin('MXN')
+        employeeList.forEach(empl => {
+            empl.wage = empl.wage * 21.50;
+        });
+      }
+    }
 
   return (
     <Grid columns='16'>
@@ -31,6 +46,10 @@ const App = () => {
             <Grid.Row>
                 <Grid columns='12'>
                     <Grid.Column computer='8' tablet='8' mobile='16'>
+                        <Header textAlign='center' as='h3'>
+                            <Icon name="users"/>
+                            You currently have {employeeList.length} users.
+                        </Header>
                         <NewConrtainer 
                         modalState={modalState} 
                         employees={employeeList} 
@@ -38,11 +57,17 @@ const App = () => {
                         setEmployees={setEmployeeList}/>
                     </Grid.Column>
                     <Grid.Column computer='8' tablet='8' mobile='16'>
-                        <Button fluid>Change currency</Button>
+                        <Header textAlign='center' as='h3'>
+                            <Icon name="money"/>
+                            Weages are on {coin}
+                        </Header>
+                        <Button fluid onClick={() => { changeCoin() }}>
+                            Change currency
+                        </Button>
                     </Grid.Column>
                 </Grid>
             </Grid.Row>
-            <Grid.Row>
+            <Grid.Row className='py-4'>
                 <TableContainer
                 employees={employeeList}
                 setEmployees={setEmployeeList}/>
